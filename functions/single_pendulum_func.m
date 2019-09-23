0e8680ece05bb08f790d25d12d29c19b02a0188e
Joshua Riddell <jridd30@gmail.com>
2019-09-24 08:31:49 +1000

    Remove generated files

--- functions/single_pendulum_func.m
+++
@@ -1,7 +0,0 @@
-function [dydx] = single_pendulum_func(t, y)
-    dydx = [0; 0];
-    T1 = y(1);
-    dT1 = y(2);
-    dydx(1) = dT1;
-    dydx(2) = (981*sin(T1))/100 - 125*sin(50*t)*sin(T1);
-end
--- functions/single_pendulum_spatial.m
+++
@@ -1,8 +0,0 @@
-function [positions] = single_pendulum_spatial(t, y)
-    T1 = y(1);
-    x0 = 0;
-    y0 = sin(50*t)/20;
-    x1 = sin(T1);
-    y1 = sin(50*t)/20 + cos(T1);
-    positions = [[x0, y0]; [x1, y1]];
-end
