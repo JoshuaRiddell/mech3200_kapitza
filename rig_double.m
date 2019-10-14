50dde96dee53fd7e63e291e5097f44b5b4f9af46
Joshua Riddell <jridd30@gmail.com>
2019-10-14 23:33:20 +1000

    Add rig test parameters

--- rig_double.m
+++ rig_double.m
@@ -9,23 +9,26 @@ setup
 % parameters to be subsituted into the model
 values = [
     m     1.0   %kg
-    l     0.95   %m
+    l     0.15   %m
     g     9.81  %m/s/s
-    w_f   220    %rad/s (base oscillation)
-    a     0.03  %m (base oscillation)
+    w_f   2*pi*31    %rad/s (base oscillation)
+    a     0.01  %m (base oscillation)
     c     0.09 %N.s (axial damping)
 ];
+
+
+
 
 generate_double_pendulum_eom(values);
 [time, state] = simulate_eoms(20, [0.1; 0; 0; 0], @double_pendulum_func);
 animate_eoms(time, state, @double_pendulum_spatial, 'rig_double');
 
-[~, FTs1] = generate_fft(time, state(:,1));
-[Fv, FTs2] = generate_fft(time, state(:,3));
-FTs = [FTs1, FTs2];
-plot_fft(Fv, FTs, 'MATLAB Model - Double Kapitza FFT', [0 50], [3*10^-6 0.2], {'Pendulum 1 Angle', 'Pendulum 2 Angle'});
-saveas(gcf, 'figures/double_fft.png');
+% [~, FTs1] = generate_fft(time, state(:,1));
+% [Fv, FTs2] = generate_fft(time, state(:,3));
+% FTs = [FTs1, FTs2];
+% plot_fft(Fv, FTs, 'MATLAB Model - Double Kapitza FFT', [0 50], [3*10^-6 0.2], {'Pendulum 1 Angle', 'Pendulum 2 Angle'});
+% saveas(gcf, 'figures/double_fft.png');
 
-clf
-plot_time_signal(time, [state(:,1), state(:,3)], 'MATLAB Model - Double Kapitza', [0 7], {'Pendulum 1 Angle', 'Pendulum 2 Angle'});
-saveas(gcf, 'figures/double_time.png');
+% clf
+% plot_time_signal(time, [state(:,1), state(:,3)], 'MATLAB Model - Double Kapitza', [0 7], {'Pendulum 1 Angle', 'Pendulum 2 Angle'});
+% saveas(gcf, 'figures/double_time.png');
--- rig_single.m
+++ rig_single.m
@@ -9,51 +9,18 @@ setup
 % parameters to be subsituted into the model
 values = [
     m     1.0   %kg
-    l     1.0   %m
+    l     0.15   %m
     g     9.81  %m/s/s
-    w_f   60    %rad/s (base oscillation)
+    w_f   2*pi*31    %rad/s (base oscillation)
     a     0.01  %m (base oscillation)
     c     0.09 %N.s (axial damping)
 ];
 
+% 0.8Hz measured
+
 
 generate_single_pendulum_eom(values);
-return
 [time, state] = simulate_eoms(10, [0.1, 0], @single_pendulum_func);
-% animate_eoms(time, state, @single_pendulum_spatial, 'single');
+animate_eoms(time, state, @single_pendulum_spatial, 'single');
 
 get_dominant_frequency(time, state(:, 1))
-
-
-% x = 50:50:500;
-% len = size(x, 2);
-% z = zeros(1, len);
-% 
-% for k = 1:len
-%     values(4,2) = x(k);
-% 
-%     generate_single_pendulum_eom(values);
-%     [time, state] = simulate_eoms(10, [0.1, 0], @single_pendulum_func);
-%     % animate_eoms(time, state, @single_pendulum_spatial, 'single');
-% 
-%     y(k) = get_dominant_frequency(time, state(:, 1));
-% end
-
-% plot(x,y);
-
-% t = time;
-% s = state(:,1);
-% % s = sin(0.5*2*pi*t) + sin(0.5*2*pi*t).*0.1.*sin(10*2*pi*t)
-% 
-% figure
-% plot(t,s);
-% 
-% 
-% figure
-% plot(Fv, log(abs(FTs(Iv))*2))
-% hold on
-% plot(Fv(Locs), log(Pks), '+r')
-% hold off
-% grid
-% 
-% 
